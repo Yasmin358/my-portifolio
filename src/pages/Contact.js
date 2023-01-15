78e93f0122313466d73a8f80976f8dfef20240a5
Yasmin358 <amorin.yasmin378@gmail.com>
2023-01-15 16:51:40 -0300

    Pagina de Contato - MOdificações

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -14,33 +14,35 @@ function Contact() {
     <section className="body">
       <main className="main" style={{ backgroundImage: `url(${pattern})` }}>
         <Header title="Contato" />
-        <div className="contactContainer">
-          <h3> Não seja um estranho! Conecte-se comigo: </h3>
-          <div className="contacts">
-          <a href="https://www.linkedin.com/in/yasminamorins/">
-            <img src={linkedin} alt="Logo-Linkedin" />
-            Linkedin
-          </a>
-          <a href="mailto:amorin.yasmin378@gmail.com">
-            <img src={Email} alt="Logo-gmail" />
-            Email
-          </a>
-          <a href="https://github.com/Yasmin358">
-            <img src={GitHub} alt="Logo-gitHub" />
-            GitHub
-          </a>
-          <a href="https://yasminamorins.itch.io/">
-            <img src={itch} alt="Logo-Itch.io" />
-            Itch.io
-          </a>
-          <a href="https://twitter.com/yasminAmorin358">
-            <img src={Twitter} alt="Logo-Twitter" />
-            Twitter
-          </a>
-          <a href="https://www.skoob.com.br/usuario/582197-yaya67">
-            <img src={Skoob} alt="Logo-Skoob" />
-            Skoob
-          </a>
+        <div className="containerMain"> 
+          <div className="contactContainer">
+            <h3> Não seja um estranho! Conecte-se comigo: </h3>
+            <div className="contacts">
+            <a href="https://www.linkedin.com/in/yasminamorins/">
+              <img src={linkedin} alt="Logo-Linkedin" />
+              Linkedin
+            </a>
+            <a href="mailto:amorin.yasmin378@gmail.com">
+              <img src={Email} alt="Logo-gmail" />
+              Email
+            </a>
+            <a href="https://github.com/Yasmin358">
+              <img src={GitHub} alt="Logo-gitHub" />
+              GitHub
+            </a>
+            <a href="https://yasminamorins.itch.io/">
+              <img src={itch} alt="Logo-Itch.io" />
+              Itch.io
+            </a>
+            <a href="https://twitter.com/yasminAmorin358">
+              <img src={Twitter} alt="Logo-Twitter" />
+              Twitter
+            </a>
+            <a href="https://www.skoob.com.br/usuario/582197-yaya67">
+              <img src={Skoob} alt="Logo-Skoob" />
+              Skoob
+            </a>
+            </div>
           </div>
         </div>
       </main>
